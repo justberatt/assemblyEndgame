16d2112f46099d250135d28451a650f26a564c6d
Berat Sakipi <berat.sakipi@gmail.com>
2025-03-22 08:22:51 +0100

    Added the status

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
-function App() {
-  
+/**
+ * Goal: Build out the main parts of our app
+ * 
+ * Challenge: Add a header with the game title
+ * and description. Startin' out easy 🙂🚶‍➡️
+ */
 
-  return (
-    <main>
-      
-    </main>
-  )
+export default function AssemblyEndgame() {
+    return (
+        <main>
+            <header>
+              <h1>Assembly: Endgame</h1>
+              <p>Guess the word within 8 attempts to keep the programming world safe from Assembly!</p>
+            </header>
+            <section className="status-section">
+                <h2>You Win!</h2>
+                <p>Well done! 🎉</p>
+            </section>
+        </main>
+    )
 }
-
-export default App
